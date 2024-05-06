d1fe2a2e9534a1fb4cb2f700763df0c5c9c35111
Sandun <sandunjayawardhana2001@gmail.com>
2024-05-06 22:31:41 +0530

    changed FE

--- frontend/src/components/miniComponents/PopularAuthors.jsx
+++ frontend/src/components/miniComponents/PopularAuthors.jsx
@@ -7,7 +7,7 @@
 //   useEffect(() => {
 //     const fetchAuthors = async () => {
 //       const { data } = await axios.get(
-//         "http://localhost:4000/api/v1/user/authors",
+//         "https://nasa-open-api-with-blog.vercel.app/api/v1/user/authors",
 //         { withCredentials: true }
 //       );
 //       setAuthors(data.authors);
